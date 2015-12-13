f70c7a7e3f024fdf2aa87ee5679e70d0b651b084
chrisbrody <chrisbrody@codifyacademy.com>
2015-12-13 15:54:33 -0800

    finished jquery slider

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-/*  */
-var spa = (function($) {
+/*jslint         browser : true, continue : true,
+    devel  : true, indent  : 2,    maxerr   : 50,
+    newcap : true, nomen   : true, plusplus : true,
+    regexp : true, sloppy  : true, vars     : true,
+    white  : true
+*/
+/*global jQuery */
+
+
+
+var spa = (function ($) {
 	// Module scope variables
 	var configMap = {
 		extended_height : 434,
 		extended_title: 'Click to retract',
 		retracted_height: 16,
 		retracted_title: 'Click to extend',
-		template_hmtl: '<div class="spa-slider"></div>'
+		template_hmtl: '<div class="spa-slider"><\/div>'
 	},
 	// Declare all other module scope variables
 	$chatSlider,
@@ -16,20 +25,26 @@ var spa = (function($) {
 	toggleSlider = function () {
 		var slider_height = $chatSlider.height()
 
-		if (slider_height === configMap.retracted_height) {
-			$chatSlider.animate({
-				height:configMap.extended_height
-			}).attr('title', configMap.extended_title)
+		if ( slider_height === configMap.retracted_height ) {
+			
+			$chatSlider
+				.animate({ height : configMap.extended_height })
+				.attr( 'title', configMap.extended_title )
 			return true
-		} else if (slider_height === configMap.retracted_height) {
-			$chatSlider.animate({
-				height:configMap.retracted_height
-			}).attr('title', configMap.extended_title)
+
+		} else if ( slider_height === configMap.extended_height ) {
+			
+			$chatSlider
+				.animate({ height:configMap.retracted_height })
+				.attr( 'title', configMap.extended_title )
 			return true
+
 		}
+		return false
 	}
 
-	onClickSlider = function ( evenet ) {
+	onClickSlider = function ( event ) {
+		console.log('clicked up')
 		toggleSlider()
 		return false
 	}
@@ -37,12 +52,14 @@ var spa = (function($) {
 	initModule = function ( $container ) {
 		$container.html( configMap.template_hmtl )
 		$chatSlider = $container.find( ' .spa-slider ')
-		$chatSlider.attr('title', configMap.retracted_title).click(onClickSlider)
+		$chatSlider
+			.attr( 'title', configMap.retracted_title )
+			.click( onClickSlider )
 		return true
 	}
 
 	return{ initModule : initModule }
-})
-$(document).ready(function() {
-	function() { spa.initModule( $('#spa') ) }
+}())
+$(document).ready(function() { 
+	spa.initModule( $('#spa') ) 
 })
